9a048d74d29c37096b74b0ef3fb2d9be58af3cea
g3rry9898 <hydrogenbond36@gmail.com>
2024-11-01 02:15:44 -0700

    l

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -1,34 +1,49 @@
 #!/usr/bin/env python3
-""" MRUCache module
+""" LFUCache module
 """
-
+from collections import defaultdict
 from base_caching import BaseCaching
 
-class MRUCache(BaseCaching):
-    """ MRUCache is a caching system with a MRU eviction policy """
+class LFUCache(BaseCaching):
+    """ LFUCache defines an LFU caching system """
 
     def __init__(self):
-        """ Initialize the cache """
+        """ Initialize the LFUCache """
         super().__init__()
-        self.order = []
+        self.frequency = defaultdict(int)
+        self.recency = defaultdict(int)
+        self.time = 0
 
     def put(self, key, item):
-        """ Add an item in the cache """
-        if key is not None and item is not None:
-            if key in self.cache_data:
-                self.order.remove(key)
+        """ Assign to the dictionary self.cache_data the item value for the key key """
+        if key is None or item is None:
+            return
+
+        self.time += 1
+        if key in self.cache_data:
             self.cache_data[key] = item
-            self.order.append(key)
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                mru_key = self.order.pop()
-                del self.cache_data[mru_key]
-                print(f"DISCARD: {mru_key}")
+            self.frequency[key] += 1
+            self.recency[key] = self.time
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                lfu_keys = [k for k, v in self.frequency.items() if v == min(self.frequency.values())]
+                lru_key = min(lfu_keys, key=lambda k: self.recency[k])
+                print("DISCARD:", lru_key)
+                del self.cache_data[lru_key]
+                del self.frequency[lru_key]
+                del self.recency[lru_key]
+
+            self.cache_data[key] = item
+            self.frequency[key] = 1
+            self.recency[key] = self.time
 
     def get(self, key):
-        """ Get an item by key """
-        if key in self.cache_data:
-            self.order.remove(key)
-            self.order.append(key)
-            return self.cache_data[key]
-        return None
+        """ Return the value in self.cache_data linked to key """
+        if key is None or key not in self.cache_data:
+            return None
+        
+        self.frequency[key] += 1
+        self.time += 1
+        self.recency[key] = self.time
+        return self.cache_data[key]
 
