7a5c7082266b76acc6a29ce88ede2988a5382d76
g3rry9898 <hydrogenbond36@gmail.com>
2024-11-05 05:38:51 -0800

    l

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+"""
+This module sets up a basic Flask app with Babel for internationalization.
+"""
+
+from flask import Flask, request
+from flask_babel import Babel, _
+
+app = Flask(__name__)
+
+# Instantiate Babel object
+babel = Babel(app)
+
+class Config:
+    """
+    Configuration class for Flask app.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+# Use Config class as config for your Flask app
+app.config.from_object(Config)
+
+@babel.localeselector
+def get_locale() -> Optional[str]:
+    """
+    Determine the best match for supported languages.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.route('/')
+def index():
+    """
+    Render the index page.
+    """
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
